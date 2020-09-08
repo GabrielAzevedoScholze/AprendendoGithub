f712796efb2af9ae3afdc3d4d21a9e4877e2b76c
Gabriel Azevedo Scholze <scholzegabriel@gmail.com>
2020-09-08 17:22:02 -0300

    Teste 1

---
+++ test1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float G1;
+	float G2;
+	float G3;
+    float media;
+	
+	cout << "informe a nota Grau 1: ";
+	cin >> G1;
+	
+	cout << endl;
+	
+	cout << "informe a nota Grau 2: ";
+	cin >> G2;
+	
+	cout << endl;
+	
+	cout << "informe a nota Grau 3: ";
+	cin >> G3;
+	
+	cout << endl;
+	
+	media = (G1 * 1 + G2 * 2 + G3 * 3) / 6;
+	
+	cout << "essa eh a sua media: " << media << endl << endl;  
+	
+//system("pouse");
+
+}
