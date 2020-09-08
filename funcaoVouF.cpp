a3438d1ae39942d828df1bf3582658be99df4f5c
Gabriel Azevedo Scholze <scholzegabriel@gmail.com>
2020-09-08 16:54:39 -0300

    funcaoVouF

---
+++ funcaoVouF.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int scopoValidacao(int x, int result){
+	
+	if(x % 2 == 0){
+		result = x
+	}
+	else{
+		
+	} 
+		
+}
+
+
+int main(void){
+	
+	int x;
+	
+	printf("Digite um valor: ");
+	scanf("%d",&x);
+	
+	int resposta = scopoValidacao (x,result);
+	
+		
+	system("pause");
+}
