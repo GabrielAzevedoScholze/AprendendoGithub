9424dd182bd6eada9a37836346788ce729222c0d
Gabriel Azevedo Scholze <scholzegabriel@gmail.com>
2020-09-08 16:51:50 -0300

    exNumParDe0a200

---
+++ exNumParDe0a200.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(void){
+	
+	int x = 0;
+	
+	for(int i = 0; i < 201; i++){
+		
+		x = i / 2;
+		
+		if(i % 2 == 0){
+			
+			printf("\n%d",i);
+			
+		} 
+		
+	}
+	
+	
+	printf("\n");
+	
+	
+	
+	
+	
+	
+	system("pause");
+}
