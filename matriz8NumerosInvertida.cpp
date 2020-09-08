c4093685dac4ec34b98c0aca05dc236ee651c0af
Gabriel Azevedo Scholze <scholzegabriel@gmail.com>
2020-09-08 16:56:50 -0300

    matriz8NumerosInvertida

---
+++ matriz8NumerosInvertida.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <locale.h>
+
+int main(void){
+	
+	setlocale(LC_ALL,"portuguese");
+	
+	int i = 0;
+	int m[8];
+	
+	while(i < 8){
+		
+		printf("Digite o número inteiro: ");
+		scanf("%d",&m[i]);
+		if(m[i]%2 == 0){
+		
+			
+		}
+		
+		i++;
+	}
+	
+	for(i = 0; i < 8; i++){
+		
+		
+		
+	}
+	
+	
+	
+	system("pause");
+}
